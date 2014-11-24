bb13fe31bf76d4ed80734079c71004d55f4dc0ec
Thyago Rafael <thyagorafael012@gmail.com>
2014-11-24 01:18:46 -0200

    Add gulp file and node modules.

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp'),
+autoprefixer = require('gulp-autoprefixer'),
+concat = require('gulp-concat'),
+cssMinify = require('gulp-cssMinify'),
+comb = require('gulp-csscomb'),
+htmlReplace = require('gulp-htmlReplace'),
+htmlMinify = require('gulp-htmlMinify'),
+rename = require('gulp-rename'),
+sass = require('gulp-sass'),
+jsMinify = require('gulp-uglify'),
+util = require('gulp-util'),
+htmlFiles = ['html/header.html', 'html/projects.html', 'html/jobs.html', 'html/skills.html', 'html/posts.html', 'html/footer.html'],
+cssFiles = 'css/*.css',
+jsFiles = 'js/*.js';
+
+gulp.task('css', function() {
+    return gulp.src('css/main.scss')
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(comb())
+    .pipe(cssMinify())
+    .pipe(rename('main.css'))
+    .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('js', function() {
+    return gulp.src('js/*.js')
+    .pipe(jsMinify())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/'))
+});
+
+gukp.task('html', function() {
+    return gulp.src(htmlFiles)
+    .pipe(htmlMinify())
+    .pipe(concat('main.html'))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('site', function() {
+    return gulp.src('main/index.html')
+    .pipe(htmlReplace({
+        'css': ['dist/main.min.css'],
+        'html': ['dist/main.min.html'],
+        'js': ['dist/main.min.js'],
+    }))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('/'))
+});
