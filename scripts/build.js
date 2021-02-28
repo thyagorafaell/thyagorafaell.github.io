2f6ddfbab18012e5414332177f6a814be0f426ed
Thyago Rafael <thyagorafael012@gmail.com>
2021-02-28 16:48:31 -0300

    Build action (#4)
    * Create build boilerplate files
    
    * Move code to source dir
    
    * Build script
    
    * New deploy action

---
+++ scripts/build.js
@@ -0,0 +1,37 @@
+var cssInliner = require('css-inliner');
+var fs = require('fs');
+var htmlMinifier = require('html-minifier').minify;
+var ncp = require('ncp').ncp;
+
+var inliner = new cssInliner({
+    directory: '.'
+});
+
+var originalHTML = fs.readFileSync('./src/index.html', 'utf8');
+
+if (!fs.existsSync('dist')) {
+    fs.mkdirSync('dist')
+}
+
+inliner.on('warning', function(warning) {
+    console.log('So this happened:', warning);
+});
+
+ncp('./src/assets', './dist', function (err) {
+    inliner
+        .inlineCSSAsync(originalHTML)
+        .then(function(inlineCSSinHTML) {
+            var minifiedHTML = htmlMinifier(inlineCSSinHTML, {
+                collapseInlineTagWhitespace: true,
+                collapseWhitespace: false,
+                minifyCSS: true,
+                removeAttributeQuotes: true
+            });
+
+            try {
+                fs.writeFileSync('./dist/index.html', minifiedHTML);
+            } catch (err) {
+                console.error(err);
+            }
+        });
+});
