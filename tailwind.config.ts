cb89946908f97a0fc9fc3273f49b9a48391806d5
Thyago Rafael <thyagorafael012@gmail.com>
2026-08-21 00:30:46 -0300

    chore: update Tailwind CSS configuration to use tailwindcss-animate import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
 	darkMode: ["class"],
@@ -104,5 +105,5 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
 } satisfies Config;
